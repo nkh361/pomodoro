011e9b4cca44d86aa8ba31afd0750f2e5c2b10f4
nkh361 <nho21@uic.edu>
2021-09-03 00:10:25 -0500

    lol

--- App.py
+++ App.py
@@ -5,8 +5,14 @@ class Pomo():
     def __init__(self, timer):
         self.timer = timer * 60
 
-    def PomoTimer(self):
+    def PomoTimer(self) -> None:
         for i in trange(self.timer):
             time.sleep(1)
+        x = input("Restart?[y/n] ")
+        if x == 'y':
+            self.PomoTimer()
+        else:
+            return 
+    
 
 Pomo(int(input("How long do ya wanna study for? "))).PomoTimer()
