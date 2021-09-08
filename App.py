9433c6ee384c8adab5a4f5a3c5ddd8ac041b2bcd
nkh361 <nho21@uic.edu>
2021-09-07 20:15:41 -0500

    break timer

--- App.py
+++ App.py
@@ -3,11 +3,16 @@ from colorama import Fore
 import time
 
 class Pomo():
-    def __init__(self, timer):
-        self.timer = timer * 60
+    def __init__(self, study_timer, break_timer):
+        self.study_timer = study_timer * 60
+        self.break_timer = break_timer * 60
+
 
     def PomoTimer(self) -> None:
-        for i in trange(self.timer, bar_format="{l_bar}%s{bar}%s{r_bar}" % (Fore.BLUE, Fore.RESET)):
+        for i in trange(self.study_timer, bar_format="{l_bar}%s{bar}%s{r_bar}" % (Fore.BLUE, Fore.RESET)):
+            time.sleep(1)
+        print("----------------------------------Break Time----------------------------------")
+        for i in trange(self.break_timer, bar_format="{l_bar}%s{bar}%s{r_bar}" % (Fore.GREEN, Fore.RESET)):
             time.sleep(1)
         x = input("Restart?[y/n] ")
         if x == 'y':
@@ -15,4 +20,7 @@ class Pomo():
         else:
             return
 
-Pomo(int(input("How long do ya wanna study for? "))).PomoTimer()
+study_time = int(input("how long do ya wanna study for? "))
+break_time = int(input("how long do ya wanna take a break for? "))
+
+Pomo(study_time, break_time).PomoTimer()
